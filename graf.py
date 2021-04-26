7c34e924ae3e856103e1c5ffbc8604831dab4e3f
e-katerina <kate.sult03@gmail.com>
2021-04-27 01:06:42 +0300

    added some docs

--- graf.py
+++ graf.py
@@ -13,22 +13,37 @@ class Graf:
         self.edges = None
         
     def get_nodes(self) -> int:
+        '''
+        :return: number of nodes
+        '''
         nodes = self.M_adiacenta.shape[0]
         return nodes
     
     def get_edges(self) -> int:
+        '''
+        :return: number of edges
+        '''
         edges = (np.sum(self.M_adiacenta) + np.trace(self.M_adiacenta))/2
         return int(edges)
     
     def set_adiacenta(self, M_adiacenta: npt.ArrayLike):
+        '''
+        :param M_adiacenta: adjacency matrix
+        '''
         self.M_adiacenta = M_adiacenta
         self.nodes = self.get_nodes()
         self.edges = self.get_edges()
     
     def get_adiacenta(self) -> npt.ArrayLike:
+        '''
+        :return: adjacency matrix
+        '''
         return self.M_adiacenta
         
     def get_incidenta(self) -> npt.ArrayLike:
+        '''
+        :return: incidence matrix
+        '''
         M_incidenta = np.array([]).reshape(self.nodes, 0)
         for i in range(self.nodes):
             for j in range(i, self.nodes):
@@ -40,12 +55,21 @@ class Graf:
         return M_incidenta
     
     def get_kirhoff(self) -> npt.ArrayLike:
+        '''
+        :return: kirhoff matrix
+        '''
         M_kirhoff = -self.M_adiacenta
         for i in range(self.nodes):
             M_kirhoff[i, i] = np.sum(self.M_adiacenta[i])
         return M_kirhoff
     
     def get_united_graph(A: Type[Graf], B: Type[Graf]) -> Type[Graf]:
+        '''
+        Merge two graphs into one
+        :param A: first graph
+        :param B: second graph
+        :return: united graph
+        '''
         A_adiacenta = A.get_adiacenta()
         B_adiacenta = B.get_adiacenta()
         
