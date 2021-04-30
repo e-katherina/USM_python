6e8d8de367e086ca8844dc6180d389fd69143514
e-katerina <kate.sult03@gmail.com>
2021-04-30 21:41:49 +0300

    simplified suma

--- calcule.py
+++ calcule.py
@@ -20,10 +20,5 @@ class Calcule:
         :param n:
         :return: sum of first n elements
         """
-        a = (np.sqrt(1 + 4 * n) - 1) / 2
-        even, odd = int(a), int(a)
-        s = even * (even + 1)
-        s += odd * (4 * odd ** 2 - 1) / 3
-        if not a.is_integer():
-            s += (n - even - odd ** 2) * (2 * even + 1)
-        return s
+        k = int((np.sqrt(1 + 4*n) - 1)/2)
+        return round(-2*k**3 / 3 - 2*k**2 + (2*n - 1/3)*k + n)
